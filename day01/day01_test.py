c31f0b37ad5227aebda6e92c655d5af15aad1638
MicSie <17578691+MicSie@users.noreply.github.com>
2022-12-01 22:19:59 +0100

    basic ensure directory in testing

--- day01/day01_test.py
+++ day01/day01_test.py
@@ -4,26 +4,31 @@ import os
 
 
 class Test_TestIncrementDecrement(unittest.TestCase):
+    def _ensure_directory(self):
+        if (os.path.basename(os.getcwd()).lower() != 'day01'):
+            os.chdir('day01')
+
     def test_part1(self):
-        os.chdir('day01')
+        self._ensure_directory()
         calories = day01.read_calories_from_file('testinput')
         self.assertEqual(day01.part1(calories), 24000)
 
     def test_part2(self):
-        os.chdir('day01')
+        self._ensure_directory()
         calories = day01.read_calories_from_file('testinput')
         self.assertEqual(day01.part2(calories), 45000)
 
     def test_read_file(self):
-        os.chdir('day01')
+        self._ensure_directory()
         self.assertEqual(day01.read_file('simpletestfile'), [
             'first', '2dn', '', '4th'])
 
     def test_sum_lines(self):
-      self.assertEqual(day01.sum_lines(['5', '5', '', '20', '', '15', '5', '4', '1', '5', '']), [10,20,30])
+        self.assertEqual(day01.sum_lines(
+            ['5', '5', '', '20', '', '15', '5', '4', '1', '5', '']), [10, 20, 30])
 
     def test_sum_lines_multiple_empty_lines(self):
-      self.assertEqual(day01.sum_lines(['5', '5', '', '', '20']), [10,20])
+        self.assertEqual(day01.sum_lines(['5', '5', '', '', '20']), [10, 20])
 
 
 if __name__ == '__main__':
