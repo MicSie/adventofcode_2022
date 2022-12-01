8416c70386b5c058c9f0aeaf71f62f8eebe2cc4f
MicSie <17578691+MicSie@users.noreply.github.com>
2022-12-01 21:21:15 +0100

    naming conventions

--- day01/day01.py
+++ day01/day01.py
@@ -1,18 +1,18 @@
-def Part1(calories: list) -> int:
+def part1(calories: list) -> int:
   return max(calories)
 
-def Part2(calories: list) -> int:
+def part2(calories: list) -> int:
   return sum(sorted(calories, reverse=True)[:3])
 
-def ReadCaloriesFromFile(fileName: str) -> list:
-  lines = ReadFile(fileName)
-  return SumLines(lines)
+def read_calories_from_file(fileName: str) -> list:
+  lines = read_file(fileName)
+  return sum_lines(lines)
 
-def ReadFile(fileName: str) -> list:
+def read_file(fileName: str) -> list:
   with open(fileName, 'r') as file:
     return [line.strip() for line in file]
 
-def SumLines(lines: list) -> list:
+def sum_lines(lines: list) -> list:
   result = []
   current = 0
   for line in lines:
@@ -28,6 +28,6 @@ def SumLines(lines: list) -> list:
   return result
 
 if __name__ == '__main__':
-  calories = ReadCaloriesFromFile('input')
-  print('Part1: '+str(Part1(calories)))
-  print('Part2: '+str(Part2(calories)))
+  calories = read_calories_from_file('input')
+  print('Part1: '+str(part1(calories)))
+  print('Part2: '+str(part2(calories)))
--- day01/day01_test.py
+++ day01/day01_test.py
@@ -4,26 +4,26 @@ import os
 
 
 class Test_TestIncrementDecrement(unittest.TestCase):
-    def test_Part1(self):
+    def test_part1(self):
         os.chdir('day01')
-        calories = day01.ReadCaloriesFromFile('testinput')
-        self.assertEqual(day01.Part1(calories), 24000)
+        calories = day01.read_calories_from_file('testinput')
+        self.assertEqual(day01.part1(calories), 24000)
 
-    def test_Part2(self):
+    def test_part2(self):
         os.chdir('day01')
-        calories = day01.ReadCaloriesFromFile('testinput')
-        self.assertEqual(day01.Part2(calories), 45000)
+        calories = day01.read_calories_from_file('testinput')
+        self.assertEqual(day01.part2(calories), 45000)
 
-    def test_ReadFile(self):
+    def test_read_file(self):
         os.chdir('day01')
-        self.assertEqual(day01.ReadFile('simpletestfile'), [
+        self.assertEqual(day01.read_file('simpletestfile'), [
             'first', '2dn', '', '4th'])
 
-    def test_Sumlines(self):
-      self.assertEqual(day01.SumLines(['5', '5', '', '20', '', '15', '5', '4', '1', '5', '']), [10,20,30])
+    def test_sum_lines(self):
+      self.assertEqual(day01.sum_lines(['5', '5', '', '20', '', '15', '5', '4', '1', '5', '']), [10,20,30])
 
-    def test_Sumlines_MultipleEmptyLines(self):
-      self.assertEqual(day01.SumLines(['5', '5', '', '', '20']), [10,20])
+    def test_sum_lines_multiple_empty_lines(self):
+      self.assertEqual(day01.sum_lines(['5', '5', '', '', '20']), [10,20])
 
 
 if __name__ == '__main__':
